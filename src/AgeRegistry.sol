aced64a0726e721445f7a89aeb7d7879c2e38120
cyber-amethyst <chukwuvictorious3@gmail.com>
2026-04-21 05:27:02 +0100

    feat: add double registration guard and independent user tests

--- src/AgeRegistry.sol
+++ src/AgeRegistry.sol
@@ -3,27 +3,42 @@ pragma solidity ^0.8.19;
 
 // Custom errors
 error AgeRegistry__AgeTooHigh();
+error AgeRegistry__AlreadyRegistered();
+
 // I want to create a contract that allows a user to register their age, and then review the users whose age are below 10 years old.
 
 contract AgeRegistry {
     mapping(address => bool) private registered;
+    mapping(address => uint256) private userAge;
 
     //function to register the user
     function register(address user, uint256 age) external {
         if (age >= 18) revert AgeRegistry__AgeTooHigh();
+        if (registered[user]) revert AgeRegistry__AlreadyRegistered();
 
         registered[user] = true;
+        userAge[user] = age;
     }
     //function to register yourself
 
     function registerMyself(uint256 age) external {
         if (age >= 18) revert AgeRegistry__AgeTooHigh();
+        if (registered[msg.sender]) revert AgeRegistry__AlreadyRegistered();
 
         registered[msg.sender] = true;
+        userAge[msg.sender] = age;
+    }
+
+    function isRegistered(address user) external view returns (bool) {
+        return registered[user];
     }
 
     //function to view the age of user now
-    function isRegistered(address user) external view returns (bool) {
-        return registered[user];
+    function getUserAge(address user) external view returns (uint256) {
+        return userAge[user];
+    }
+
+    function getMyAge() external view returns (uint256) {
+        return userAge[msg.sender];
     }
 }
--- test/AgeRegistryTest.t.sol
+++ test/AgeRegistryTest.t.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
-import {AgeRegistry, AgeRegistry__AgeTooHigh} from "../src/AgeRegistry.sol";
+import {AgeRegistry, AgeRegistry__AgeTooHigh, AgeRegistry__AlreadyRegistered} from "../src/AgeRegistry.sol";
 
 contract AgeRegistryTest is Test {
     AgeRegistry public registry;
     address internal user = makeAddr("user");
+    address internal user1 = makeAddr("user1");
+    address internal user2 = makeAddr("user2");
 
     function setUp() external {
         registry = new AgeRegistry();
@@ -39,5 +41,44 @@ contract AgeRegistryTest is Test {
         vm.expectRevert(AgeRegistry__AgeTooHigh.selector);
         vm.prank(user);
         registry.registerMyself(18);
+    }
+
+    function test_StoresAgeAfterRegister() external {
+        registry.register(user, 7);
+        assertEq(registry.getUserAge(user), 7);
+    }
+
+    function test_StoreMyAgeAfterIRegisterMyself() external {
+        vm.startPrank(user);
+        registry.registerMyself(15);
+        assertEq(registry.getMyAge(), 15);
+        vm.stopPrank();
+    }
+
+    function test_UnregisteredUserReturns0InGetUserAge() external view {
+        assertEq(registry.getUserAge(user), 0);
+    }
+
+    function test_AgeRevertsWhenAlreadyRegistered() external {
+        registry.register(user, 17);
+        vm.expectRevert(AgeRegistry__AlreadyRegistered.selector);
+        registry.register(user, 17);
+    }
+
+    function test_AgeRevertsWhenAlreadyRegisteredMyself() external {
+        vm.prank(user);
+        registry.registerMyself(17);
+        vm.expectRevert(AgeRegistry__AlreadyRegistered.selector);
+        vm.prank(user);
+        registry.registerMyself(17);
+    }
+
+    function test_TwoUsersCanRegisterIndependently() external {
+        registry.register(user1, 12);
+        registry.register(user2, 14);
+        bool registered = registry.isRegistered(user1) && registry.isRegistered(user2);
+        assertEq(registry.getUserAge(user1), 12);
+        assertEq(registry.getUserAge(user2), 14);
+        assertEq(registered, true);
     }
 }
